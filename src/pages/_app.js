2396d985df816e6eed06e5fb9cb4754f3a297824
dr-robotnic456 <ernesteshun456@gmail.com>
2023-07-24 22:22:45 +0000

    update2 commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 import '@/styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 
-export default function App({ Component, pageProps : {session, ...pageProps} }) {
+export default function App({ Component,  ...pageProps} ) {
   return (
-    <SessionProvider session={session}>
       <ThemeProvider enableSystem={true} attribute='class'>
         <Component {...pageProps} />
       </ThemeProvider>
-    </SessionProvider>
 
   )
 }

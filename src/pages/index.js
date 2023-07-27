05f5c369651ec1c5e7045ffdfbd4325cade523e2
dr-robotnic456 <ernesteshun456@gmail.com>
2023-07-27 10:14:09 +0000

    update3 commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Homepage</title>
     </Head>
-    <div>
+    <div className='w-full'>
       {/* {session?( */}
         <HomePage handleSignOut = {handleSignOut}/>
       {/* ): (<Login setSession = {setSession}/>)} */}

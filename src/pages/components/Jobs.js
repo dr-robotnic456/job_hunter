ed1c3a394edee613b22cd52d23eef5880f7386b2
dr-robotnic456 <ernesteshun456@gmail.com>
2023-07-27 10:32:37 +0000

    update3 commit

--- src/pages/components/Jobs.js
+++ src/pages/components/Jobs.js
@@ -1,29 +1,23 @@
+// Jobs.js
+
 import React from 'react';
 import CategoryCard from '../components/CategoryCard';
 
 const Jobs = ({ result, handleClick, category, isActive, setIsActive }) => {
-  if (result && result.length === 0) {
-    return (
-      <div>
-        <div id='job' className='bg-white flex-col flex h-screen  flex-wrap w-full overflow-hidden max-w-screen-lg flex-start gap-1 dark:bg-black'>
-          <CategoryCard handleClick = {handleClick} category = {category} isActive={isActive} setIsActive={setIsActive}/>
-        </div>
-        <div className='text-red-700 items-center flex flex-col justify-center text-center text-9xl uppercase dark:bg-black '>
-          No Data Found!
-        </div>
+  return (
+    <div id='job' className='bg-white flex-col h-screen w-full overflow-hidden max-w-screen-lg flex-start gap-1 dark:bg-black'>
+      <CategoryCard handleClick={handleClick} category={category} isActive={isActive} setIsActive={setIsActive} />
+      <div className="flex flex-wrap">
+        {result && result.length === 0 ? (
+          <div className='text-red-700 items-center flex flex-col justify-center text-center text-9xl uppercase dark:bg-black'>
+            No Data Found!
+          </div>
+        ) : (
+          result
+        )}
       </div>
-    );
-  } else {
-    return (
-      <div id='job' className='bg-white flex-col h-screen flex flex-wrap  w-full overflow-hidden max-w-screen-lg flex-start gap-1 dark:bg-black'>
-        <CategoryCard handleClick = {handleClick} category = {category} isActive={isActive} setIsActive={setIsActive}/>
-        <div className="flex">
-          {result}
-        </div>
-      </div>
-    );
-  }
-
+    </div>
+  );
 };
 
 export default Jobs;

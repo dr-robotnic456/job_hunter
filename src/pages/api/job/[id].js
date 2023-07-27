565babc42bfe39e8a86921faf144705d98da0271
dr-robotnic456 <ernesteshun456@gmail.com>
2023-07-27 10:59:08 +0000

    update3 commit

--- src/pages/api/job/[id].js
+++ src/pages/api/job/[id].js
@@ -8,7 +8,7 @@ export default async function handler(req,res){
 
     switch (method) {
         case "GET":
-            const job = await Job.findById(req.query.id).populate("user");
+            const job = await Job.findById(req.query.id);
             if(!job) return res.status(400).json({message:"No data found"})
             return res.status(200).json(job)
             
